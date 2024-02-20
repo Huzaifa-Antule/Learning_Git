7f225e56084a0b19c90c4fa04f22e0df63d1bd1f
root <root@ip-172-31-28-40.ap-southeast-1.compute.internal>
2024-02-20 05:57:49 +0000

    Some Changes are made.

---
+++ Git_Github_Learning/Demo.py
@@ -0,0 +1,15 @@
+print("Hello world")
+print("writing This Code in Windows GitBash and Later Merging with Master")
+
+#Program for priting Even numbers from  given range.
+def Print_Even(Range):
+    try:
+        num = 0
+        for i in range(num,Range,2):
+            if i ==0:
+                pass
+            else:
+                print(f"Number : {i}")
+    except Exception as e:
+        print("Error : ",e)
+Print_Even(50)
